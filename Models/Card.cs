99ebe14e8c3f4d6a73966b79d359b4843b89039e
WILLNET\nick.chan <nick.chan@williams.uk.com>
2024-11-13 16:49:20 +0000

    Add project files.

---
+++ Models/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace PTCGP.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string CardName { get; set; } // The descriptive name of the card
+        public string DisplayName { get; set; } // The formatted name like "A1 001"
+        public bool IsOwned { get; set; } = false; // Whether the card is owned
+    }
+}
