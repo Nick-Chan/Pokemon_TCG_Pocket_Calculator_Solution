92c0145190e57dd787f811f17d174b46073c03bb
WILLNET\nick.chan <nick.chan@williams.uk.com>
2024-11-14 16:53:22 +0000

    Added various calculators, card totals and UI improvements such as small/medium/large grid settings.

--- Models/Card.cs
+++ Models/Card.cs
@@ -3,8 +3,8 @@
     public class Card
     {
         public int Id { get; set; }
-        public string CardName { get; set; } // The descriptive name of the card
-        public string DisplayName { get; set; } // The formatted name like "A1 001"
-        public bool IsOwned { get; set; } = false; // Whether the card is owned
+        public string CardName { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsOwned { get; set; } = false;
     }
 }
